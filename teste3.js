c4117332eb69e57dec554f349a5224b2a7b33886
Giselly Reis <gisellyareis@gmail.com>
2023-06-13 11:30:52 -0300

    resolve teste 3

--- teste3.js
+++ teste3.js
@@ -1,15 +1,31 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
 
-module.exports = function(req, res) {
-  
-    var name =  req.query.name;
+const fs = require('fs');
+const path = require('path');
+const dataFilePath = path.resolve(__dirname, 'fakeData.js');
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
-    }
+const removeUser = (req, res, next) => {
+  const name = req.query.name;
 
-    res.send("success");
+  const index = data.findIndex(user => user.name === name);
+  if (index !== -1) {
+    data.splice(index, 1);
 
+    const updatedDataContent = `module.exports = ${JSON.stringify(data, null, 2)};`;
+
+    fs.writeFile(dataFilePath, updatedDataContent, err => {
+      if (err) {
+        res.status(500).send('Erro ao remover usuário');
+        return;
+      }
+
+      res.send('Usuário removido com sucesso!');
+    });
+  } else {
+    res.status(404).send('Usuário não encontrado');
+  }
+};
+
+module.exports = {
+  removeUser
 };
