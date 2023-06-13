ae7a7d957ae25d5338d5c8c246a5d75ddf8f6eb0
Giselly Reis <gisellyareis@gmail.com>
2023-06-13 11:28:02 -0300

    resolve teste 1

--- teste1.js
+++ teste1.js
@@ -1,24 +1,21 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
 
-const getUser = ( req, res, next ) => {
-    
-    var name =  req.query.name;
+const getUser = (req, res, next) => {
+  const name = req.query.name;
+  const user = data.find((user) => user.name === name);
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
-        }
-    }
-
+  if (user) {
+    res.send(user);
+  } else {
+    res.sendStatus(404).send("Usuário não encontrado");
+  }
 };
 
-const getUsers = ( req, res, next ) => {
-    
-    res.send(data);
-    
+const getUsers = (req, res, next) => {
+  res.send(data);
 };
 
 module.exports = {
-    getUser,
-    getUsers
+  getUser,
+  getUsers
 };
