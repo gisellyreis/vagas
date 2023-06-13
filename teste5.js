0e4b7635b77423c695a07f7600a5de05c652c3b0
Giselly Reis <gisellyareis@gmail.com>
2023-06-13 13:11:08 -0300

    resolve teste 5

--- teste5.js
+++ teste5.js
@@ -1,9 +1,18 @@
+const data = require("./fakeData");
 
+const consultReadedUser = (req, res, next) => {
+    const name =  req.query.name;
+    const user = data.find((user) => user.name === name);
 
-module.exports = function(req, res){
-    
-    var name =  req.query.name;
+    if (user) {
+      const readed = user.readed || 0;
+      res.send("Usuário " +  name  + "  foi lido " + readed + " vezes.");
+    } else {
+      res.status(404).send("Usuário não encontrado");
+    }
 
-    res.send("Usuário " +  name  + "  foi lido 0 vezes.");
+};
 
+module.exports = {
+  consultReadedUser
 };
